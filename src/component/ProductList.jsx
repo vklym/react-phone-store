e8a45fe07071b15ae2c4116d01cb463e13c720b6
vklym <klym2612@gmail.com>
2019-05-27 16:41:59 +0300

    add some change

--- src/component/ProductList.jsx
+++ src/component/ProductList.jsx
@@ -9,7 +9,7 @@ export default class ProductList extends Component {
             <>
                 <div className='py-5'>
                     <div className="container">
-                        <Title name='ourrrrrrr' title='products' />
+                        <Title name='our' title='products' />
                         <div className="row">
                             <ProductConsumer>
                                 { 
